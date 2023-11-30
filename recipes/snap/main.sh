6ba8852998fcd9815f1ec5c578f315aa52a0ae17
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2023-12-01 00:19:52 +0200

    Add updates

--- recipes/snap/main.sh
+++ recipes/snap/main.sh
@@ -1,8 +1,13 @@
 source recipes/helpers/main.sh
 
 function run {
+  update_config
   exit_if_installed snap
   install
+}
+
+function update_config {
+  ln -sf $MASHINA_SOURCE/recipes/snap/init/snap.sh $MASHINA_INIT
 }
 
 function install_debian {
