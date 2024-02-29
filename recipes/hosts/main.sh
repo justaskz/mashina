10256dbcf77f69a0162e019319aabb192c6d1ada
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2024-02-29 16:28:40 +0200

    Add updates

--- recipes/hosts/main.sh
+++ recipes/hosts/main.sh
@@ -3,13 +3,14 @@ function run {
 }
 
 function install {
-  URL="https://someonewhocares.org/hosts/hosts"
+  # local URL="https://someonewhocares.org/hosts/hosts"
+  local HOSTS_FILE="$MASHINA_SOURCE/recipes/hosts/files/hosts"
 
-  HOSTS_FILE="recipes/hosts/files/hosts"
-  cat $HOSTS_FILE > $MASHINA_TMP/hosts
-  echo "" >> $MASHINA_TMP/hosts
-  curl $URL >> $MASHINA_TMP/hosts
-  sudo mv $MASHINA_TMP/hosts /etc/hosts
+  cat "$HOSTS_FILE" > /etc/hosts
+  # echo "" >> "$MASHINA_TMP/hosts"
+  # curl $URL >> "$MASHINA_TMP/hosts"
+
+  # sudo mv "$MASHINA_TMP/hosts" /etc/hosts
 }
 
 run
